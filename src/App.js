7966f3944fc95088d43f1304e6284c68025edb96
MestreSol <mb.muitobosta@gmail.com>
2024-10-29 18:22:44 -0300

    add Menu Superior

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <Router>
-      <Route path="/" component={Home} />
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/components/commum/Menu_Superior_Comum.js
@@ -0,0 +1,7 @@
+const Menu_Superior_Comum = () => {
+    return (
+        <div>
+            <h1>Menu Superior Comum</h1>
+        </div>
+    )
+}
