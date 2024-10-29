0fc96443b85c48685da3ee3f6b350ed7a215167d
MestreSol <mb.muitobosta@gmail.com>
2024-10-29 13:36:28 -0300

    Gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
 
 // Clear dist folder
@@ -9,8 +8,7 @@ gulp.task('clean', function() {
     return gulp.src('./public/dist/*', { read: false })
         .pipe(clean());
 });
-
-// Compile LESS files from /less into /css
+// files from /less into /css
 gulp.task('less', function(cb) {
     gulp.src('./public/less/*.less')
         .pipe(less())
@@ -18,22 +16,25 @@ gulp.task('less', function(cb) {
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
+gulp.task('minify-css', gulp.series('less', function() {
     return gulp.src('./public/dist/css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('./public/dist/css'));
-});
+}));
 
 // Image compressor
-gulp.task('image', function() {
-    gulp.src('./public/img/*')
+gulp.task('image', async function() {
+    const imagemin = (await import('gulp-imagemin')).default;
+    return gulp.src('./public/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./public/dist/img'));
 });
 
 // Watch for changes in files
 gulp.task('watch', function() {
-    gulp.watch('./public/less/*.less', ['less']);
-    gulp.watch('./public/dist/css/*.css', ['minify-css']);
-    gulp.watch('./public/img/*', ['image']);
+    gulp.watch('./public/less/*.less', gulp.series('less', 'minify-css'));
+    gulp.watch('./public/img/*', gulp.series('image'));
 });
+
+// Default task
+gulp.task('default', gulp.series('clean', 'less', 'minify-css', 'image', 'watch'));
