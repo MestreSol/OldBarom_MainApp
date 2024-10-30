d8bc95c2246df80b6aea2bb0bf7f1baa95a96f38
MestreSol <mb.muitobosta@gmail.com>
2024-10-30 12:29:19 -0300

    Configure Next

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import { Html, Main, NextScript } from "next/document";
+import Head from "next/head";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head></Head>
+            <body>
+                <Main></Main>
+                <NextScript></NextScript>
+            </body>
+        </Html>
+    );
+}
